58b1296806f4ad09cb75e95ad959004c00ac7e31
jhojho97 <149947133+jhojho97@users.noreply.github.com>
2025-01-19 11:56:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.write(
     """
 )
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
 name_on_order = st.text_input("Name on Smoothie")
